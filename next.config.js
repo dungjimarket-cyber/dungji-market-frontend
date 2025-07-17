ce979da55f56b88742b89111e52ad925138ef465
crom <holyfavor@gmail.com>
2025-07-17 12:47:09 +0900

    feat: add images.samsung.com to Next.js image domains whitelist

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   images: {
     remotePatterns: [{ hostname: 'k.kakaocdn.net' }, { hostname: 'lh3.googleusercontent.com' }, { hostname: 'image.shop.kt.com' }, { hostname: 'img.danawa.com' }],
-    domains: ['k.kakaocdn.net', 'lh3.googleusercontent.com', 'image.shop.kt.com', 'img.danawa.com', 'dungjimarket.s3.amazonaws.com'],
+    domains: ['k.kakaocdn.net', 'lh3.googleusercontent.com', 'image.shop.kt.com', 'img.danawa.com', 'dungjimarket.s3.amazonaws.com', 'images.samsung.com'],
   },
   // TypeScript 타입 검사 활성화
 }
